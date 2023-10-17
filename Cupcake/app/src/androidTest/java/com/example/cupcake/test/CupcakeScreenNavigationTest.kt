8f38f7bd83c345b0f60fec133a0a056112b19ea5
Undiy <undiy@yandex.ru>
2023-10-17 13:40:38 +0500

    Cupcake: test no backbutton on start screen

--- Cupcake/app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
+++ Cupcake/app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -3,11 +3,12 @@ package com.example.cupcake.test
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import com.example.cupcake.CupcakeApp
 import com.example.cupcake.CupcakeScreen
-import org.junit.Assert.assertEquals
+import com.example.cupcake.R
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -31,9 +32,12 @@ class CupcakeScreenNavigationTest {
 
     @Test
     fun cupcakeNavHost_verifyStartDestination() {
-        assertEquals(
-            CupcakeScreen.Start.name,
-            navController.currentBackStackEntry?.destination?.route
-        )
+        navController.assertCurrentRouteName(CupcakeScreen.Start.name)
+    }
+
+    @Test
+    fun cupcakeNavHost_verifyBackNavigationNotShownOnStartOrderScreen() {
+        val backText = composeTestRule.activity.getString(R.string.back_button)
+        composeTestRule.onNodeWithContentDescription(backText).assertDoesNotExist()
     }
 }

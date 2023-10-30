38656c2b7cac49685d3fe1a1da6ffcde8e4548f4
Undiy <undiy@yandex.ru>
2023-10-30 16:06:18 +0500

    Inventory: delete DB test

--- Inventory/app/src/androidTest/kotlin/ItemDaoTest.kt
+++ Inventory/app/src/androidTest/kotlin/ItemDaoTest.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -80,4 +81,13 @@ class ItemDaoTest {
         assertEquals(allItems[1], Item(2, "Bananas", 5.0, 50))
     }
 
+    @Test
+    @Throws(Exception::class)
+    fun daoDeleteItems_deletesAllItemsFromDB() = runBlocking {
+        addTwoItemsToDb()
+        itemDao.delete(item1)
+        itemDao.delete(item2)
+        val allItems = itemDao.getAllItems().first()
+        assertTrue(allItems.isEmpty())
+    }
 }
